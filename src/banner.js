09a8c12a2cfdbaae29ee6d2b8e1793f7a75031ac
britz <brittochristy@gmail.com>
2026-06-06 17:28:04 +0530

    style: use custom ascii banner

--- src/banner.js
+++ src/banner.js
@@ -1,10 +1,6 @@
-import figlet from "figlet";
-
 export function showBanner() {
-  const text = figlet.textSync("zgit", {
-    font: "Standard",
-    horizontalLayout: "default",
-    verticalLayout: "default",
-  });
-  return `${text}\n  A beautifully colored git alternative`;
+  return `▘▗ 
+▀▌▛▌▌▜▘
+▙▖▙▌▌▐▖
+  ▄▌`;
 }
