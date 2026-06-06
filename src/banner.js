47dbb790d8bbf0058a622b35f6eb4d5ef3c57b26
britz <brittochristy@gmail.com>
2026-06-06 17:28:58 +0530

    feat: render custom zgit ascii logo bold and unbolded

--- src/banner.js
+++ src/banner.js
@@ -1,6 +1,9 @@
 export function showBanner() {
-  return `▘▗ 
+  const banner = `▘▗ 
 ▀▌▛▌▌▜▘
 ▙▖▙▌▌▐▖
   ▄▌`;
+
+  console.log(`\x1b[1m${banner}\x1b[0m`);
+  console.log(`\x1b[1m  zgit\x1b[0m\n`);
 }
